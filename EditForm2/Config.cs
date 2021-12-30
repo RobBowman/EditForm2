12f0d698c98033d7cef7d2feee529e6bb29c6aa8
BizTalkers <rob@biztalkers.com>
2021-12-30 14:57:17 +0000

    Add project files.

---
+++ EditForm2/Config.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EditForm2
+{
+    public class ConfigViewModel
+    {
+        public string BusinessArea { get; set; } = "";
+
+        public bool RouteInboundSmsToEmail { get; set; }
+    }
+
+}
